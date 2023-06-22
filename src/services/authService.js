35f9b0e552086f090b31686cb4ca5a63ea79fad8
SsTommyChensS <tranquoctunglun@gmail.com>
2023-06-22 14:29:46 +0700

    Services - authService: Using axiosConfig

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,11 +1,18 @@
-import axios from "axios";
+import  axiosConfig from "./axiosConfig"
 
-const base_url = process.env.NEXT_PUBLIC_API_BASE_URL;
-
+//Register
 export function signup(data) {
-    return axios.post(`${base_url}/auth/signup`, data);
+    return axiosConfig.post(`/auth/signup`, data);
 }
-
+//Login
 export function login(data) {
-    return axios.post(`${base_url}/auth/login`, data);
+    return axiosConfig.post(`/auth/login`, data);
+}
+//Logout
+export function logout() {
+    return axiosConfig.get(`/auth/logout`);
+}
+//Renew token using refreshToken (cookie)
+export function renewToken() {
+    return axiosConfig.get(`/auth/renew_token`);
 }
