769e4a89cbca7d49f29fe3f889d748214f8b67d0
SsTommyChensS <tranquoctunglun@gmail.com>
2023-06-22 14:35:26 +0700

    DashboardNavbar - Add logout and redirect back to home

--- src/components/Navbar/DashboardNavbar.js
+++ src/components/Navbar/DashboardNavbar.js
@@ -2,6 +2,10 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import { deleteCookie } from "cookies-next";
+
+import { logout } from "@/services/authService";
+import { redirect } from "next/dist/server/api-utils";
 
 const handleNavbarToggle = () => {
     let navbar_menu = document.getElementById("navbar-default");
@@ -12,32 +16,49 @@ const handleNavbarToggle = () => {
     }
 }
 
+const handleLogout = () => {
+    logout()
+        .then(response => {
+            const success_message = response.data.message;
+            alert(success_message);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+    //Remove accessToken in cookie
+    deleteCookie("token");
+    redirect("/home");
+}
+
 const DashboardNavbar = () => {
     const username = "QTung1999";
 
     return (
-        <nav className="bg-white border-gray-200 dark:bg-gray-900">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="#" className="flex items-center">
-                    <Image src="/local_project_icon.svg" width={40} height={40} className="mr-3" alt="Local Project Management icon"/>
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Dashboard</span>
-                </a>
-                <button onClick={handleNavbarToggle} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                    <span className="sr-only">Open main menu</span>
-                    <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-                </button>
-                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        <li>
-                            <Link href="" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Hello <span className="text-red-700">{username}</span></Link>
-                        </li>
-                        <li>
-                            <Link href="home" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout <Image className="inline ml-2" src="/logout.svg" width={20} height={20} alt="Logout icon"/></Link>
-                        </li>
-                    </ul>
+        <div className="navbar">
+            <nav className="bg-white border-gray-200 dark:bg-gray-900">
+                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <a href="#" className="flex items-center">
+                        <Image src="/local_project_icon.svg" width={40} height={40} className="mr-3" alt="Local Project Management icon"/>
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Dashboard</span>
+                    </a>
+                    <button onClick={handleNavbarToggle} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                        <span className="sr-only">Open main menu</span>
+                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+                    </button>
+                    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                            <li>
+                                <Link href="" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Hello <span className="text-red-700">{username}</span></Link>
+                            </li>
+                            <li>
+                                <Link onClick={handleLogout} href="/" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout <Image className="inline ml-2" src="/logout.svg" width={20} height={20} alt="Logout icon"/></Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
     )
 }
 
