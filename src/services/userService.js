d1f7649725f3270c96b8ecd03ae31f6a6f55245c
SsTommyChensS <tranquoctunglun@gmail.com>
2023-06-26 18:37:52 +0700

    Services - Add userService

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,3 +4,13 @@ import  axiosConfig from "./axiosConfig"
 export function getYourProfile () {
     return axiosConfig.get("/user/your-profile/get");
 }
+
+export function updateProfile (user_id, data) {
+    return axiosConfig.put(`/user/profile/${user_id}/update`, data);
+}
+
+export function uploadAvatar (data) {
+    return axiosConfig.post('/user/upload-avatar', data, {
+        headers: { "Content-Type": "multipart/form-data"}
+    });
+}
