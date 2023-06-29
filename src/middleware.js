51dec43fbb59ba431f234aeca802e9868dcab897
SsTommyChensS <tranquoctunglun@gmail.com>
2023-06-29 12:17:13 +0700

    Middleware - Add renewToken

--- src/middleware.js
+++ src/middleware.js
@@ -1,40 +1,44 @@
-import { setCookie, getCookie } from "cookies-next";
+import { setCookie, getCookie, hasCookie } from "cookies-next";
 
 import { NextResponse } from 'next/server'
 import { renewToken } from "./services/authService";
 
-export function middleware(req) {
-  const refreshToken = req.cookies.get("jwt")?.value;
+export async function middleware(req) {
   //Check if user logged in -> Yes: redirect to /dashboard
   if (req.nextUrl.pathname.startsWith("/home") || req.nextUrl.pathname.startsWith('/register') || req.nextUrl.pathname.startsWith('/login')) {
-    if(refreshToken !== undefined) {
-      //Check accessToken (If accessToken in cookie expired -> Renew)
-      const accessToken = getCookie("token");
-      if(accessToken === undefined) {
-        renewToken()
-          .then(response => {
-            const new_accessToken = response.data.data.accessToken;
-  
-            setCookie("token", new_accessToken, {
-              maxAge: 60 * 60 //1h
-          });
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      }
-      const url = req.nextUrl.clone();
-      url.pathname = "/dashboard";
-      return NextResponse.redirect(url, req.url);
-    }
+    if(req.cookies.has("jwt")) {
+        const url = req.nextUrl.clone();
+        url.pathname = "/dashboard";
+        return NextResponse.redirect(url, req.url);
+    }   
   }
   //Check if user logged in -> No: redirect to /login
   if(req.nextUrl.pathname.startsWith('/dashboard')) {
-    if(refreshToken === undefined) {
+    if(!req.cookies.has("jwt")) {
       const url = req.nextUrl.clone();
       url.pathname = "/login";
       return NextResponse.redirect(url, req.url);
     }
+    //Check accessToken
+      if(!req.cookies.has("token")) {
+        //Renew token using refresh token
+        let new_accessToken;
+        const refreshToken = req.cookies.get("jwt").value;
+        await renewToken(refreshToken)
+          .then(res => res.json())
+          .then(data => {
+              new_accessToken = data.data.accessToken;
+          })
+          
+          const response = NextResponse.next();
+          response.cookies.set({
+            name: "token",
+            value: new_accessToken,
+            maxAge: 60 * 60
+          });
+
+          return response;
+	  }
   }
 }
  
