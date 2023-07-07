ac715975ac121eeb8f3e868ca11e001b550c6f6f
SsTommyChensS <tranquoctunglun@gmail.com>
2023-07-07 15:11:05 +0700

    Items - Add TitleHeading

---
+++ src/components/Items/TitleHeading.js
@@ -0,0 +1,14 @@
+const TitleHeading = ({ titleMessage, item, setOption }) => {
+    return (
+        <div className="list-attachments__heading flex justify-between p-3 gap-x-10 bg-orange-100 text-lime-800 font-serif capitalize tracking-wide italic font-bold text-lg rounded-lg">
+                <h1 className="list-attachments__title">
+                    {titleMessage} <span className="ml-2 text-red-700 underline text-2xl">{item}</span>
+                </h1>
+                <button onClick={() => setOption(0)}>
+                    <img src="/cancel.svg" width={30} height={30} alt="Cancel icon"/>
+                </button>
+        </div>   
+    )
+}
+
+export default TitleHeading;
