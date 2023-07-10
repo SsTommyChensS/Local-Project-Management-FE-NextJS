6a1762934d6c1a84f723ac1364449c38035d126c
SsTommyChensS <tranquoctunglun@gmail.com>
2023-07-10 22:02:00 +0700

    My-projects: Add handleOpenListComments

--- src/app/(dashboard)/dashboard/my-projects/page.js
+++ src/app/(dashboard)/dashboard/my-projects/page.js
@@ -6,6 +6,7 @@ import style from './myprojects.module.css';
 import Pagination from '@/components/Pagination/Pagination';
 import AddProject from '@/components/Project/AddProject';
 import DetailProject from '@/components/Project/DetailProject';
+import ListComments from '@/components/Comment/ListComments';
 import ListAttachments from '@/components/Attachment/ListAttachments';
 
 import { getMyProjects, getMyProjectsByStatus, getMyProjectsByTitle, removeProject } from '@/services/projectService';
@@ -20,6 +21,7 @@ const MyProjects = () => {
     const removeProjectModal = useRef();
     const detailProjectElement = useRef(null);
     const addProjectElement = useRef(null);
+    const commentElement = useRef(null);
     const attachmentElement = useRef(null);
     //Toggle remove project modal
     const handleOpenRemoveProjectModal = (data) => {
@@ -38,7 +40,7 @@ const MyProjects = () => {
                 <button onClick={() => handleOpenRemoveProjectModal(project)} className="bg-red-700 hover:bg-red-800 text-white p-2 rounded-lg w-full">Remove <img src="/remove.svg" className="inline ml-2 w-3 h-3"/></button>
                 <button className="bg-pink-700 hover:bg-pink-800 text-white p-2 rounded-lg w-full">Members <img src="/member.svg" className="inline ml-2 w-4 h-4"/></button>
                 <button className="bg-green-700 hover:bg-green-800 text-white p-2 rounded-lg w-full">Tasks <img src="/task.svg" className="inline ml-2 w-4 h-4"/></button>
-                <button className="bg-red-400 hover:bg-red-500 text-white p-2 rounded-lg w-full">Comments <img src="/comment.svg" className="inline ml-2 w-4 h-4"/></button>
+                <button onClick={() => handleOpenListComments(project)} className="bg-red-400 hover:bg-red-500 text-white p-2 rounded-lg w-full">Comments <img src="/comment.svg" className="inline ml-2 w-4 h-4"/></button>
                 <button onClick={() => handleOpenListAttachments(project)} className="bg-orange-400 hover:bg-orange-500 text-white p-2 rounded-lg w-full">Attachments <img src="/attachment.svg" className="inline ml-2 w-4 h-4"/></button>
             </>
         )
@@ -159,6 +161,12 @@ const MyProjects = () => {
         setSelectedProject(data);
         detailProjectElement.current?.scrollIntoView({behavior: "smooth"});
     };
+    //List comments
+    const handleOpenListComments = (data) => {
+        setOption(5);
+        setSelectedProject(data);
+        commentElement.current?.scrollIntoView({behavior: "smooth"});
+    }
     //List attachments
     const handleOpenListAttachments = (data) => {
         setOption(6);
@@ -251,6 +259,7 @@ const MyProjects = () => {
             </div>
             { option == 1 && <AddProject ref={addProjectElement} setOption={setOption}/> }
             { option == 2 && <DetailProject ref={detailProjectElement} project={selectedProject} setOption={setOption}/> }
+            { option == 5 && <ListComments ref={commentElement} project={selectedProject} setOption={setOption}/> }
             { option == 6 && <ListAttachments ref={attachmentElement} project={selectedProject} setOption={setOption}/> }
         </div>
     )
