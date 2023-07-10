86bdf8b837be753606f37d1d9381dd32fc86fe61
SsTommyChensS <tranquoctunglun@gmail.com>
2023-07-10 21:55:21 +0700

    Items - TitleHeading: Adjust className attribute

--- src/components/Items/TitleHeading.js
+++ src/components/Items/TitleHeading.js
@@ -1,7 +1,7 @@
 const TitleHeading = ({ titleMessage, item, setOption }) => {
     return (
-        <div className="list-attachments__heading flex justify-between p-3 gap-x-10 bg-orange-100 text-lime-800 font-serif capitalize tracking-wide italic font-bold text-lg rounded-lg">
-                <h1 className="list-attachments__title">
+        <div className="title-heading__section flex justify-between p-3 gap-x-10 bg-orange-100 text-lime-800 font-serif capitalize tracking-wide italic font-bold text-lg rounded-lg">
+                <h1 className="title-headding__title">
                     {titleMessage} <span className="ml-2 text-red-700 underline text-2xl">{item}</span>
                 </h1>
                 <button onClick={() => setOption(0)}>
